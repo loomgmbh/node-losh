4cb8bc5648ba254387a2167594cd67e9a1fb217a
Terra Zero <terrazerocore@gmail.com>
2020-11-06 10:10:34 +0100

    test deploy

--- src/templates/deploy.js
+++ src/templates/deploy.js
@@ -2,32 +2,52 @@
  * @this {Executable}
  */
 module.exports = async function() {
-  this.log.note('Deploy');
-  let result = null;
-  
-  const hash = await this.git.getCurrentHash();
-  const path = this.path('extension', 'currenthash.txt');
-  this.log.note('Mark current hash [!hash] here [!path]', {'!path': this.relative(path), '!hash': hash});
-  result = await this.write(path, hash, true);
-  if (result.error) return;
+  try {
+    this.strict(true);
+    this.log.note('Deployment [type]', {type: this.args.type});
+    console.log();
 
-  this.log.note('Update code; Update config; Composer install; Compile theme');
-  console.log();
-  this.log.note('Get new code version');
-  result = await this.git.pull();
-  if (result.error) return;
-  const newHash = await this.git.getCurrentHash();
-  this.log.note('New hash [!hash]', {'!hash': newHash});
-  console.log();
-  this.log.note('Update composer');
-  result = await this.composer.install();
-  if (result.error) return;
-  console.log();
-  this.log.note('Update config');
-  await this.drush.cr();
-  await this.drush.cim(true);
-  console.log();
-  // gulp
+    const hash = await this.git.getCurrentHash();
+    const path = this.path('extension', 'currenthash.txt');
+
+    this.log.note('Update code; Update config; Composer install; Compile theme');
+    console.log();
+
+    this.log.note('Get new code version');
+    await this.git.pull();
+    const newHash = await this.git.getCurrentHash();
+
+    if (hash !== newHash) {
+      this.log.note('Mark current hash [!hash] here [!path]', {'!path': this.relative(path), '!hash': hash});
+      await this.write(path, hash, true);
+      this.log.note('New hash [!hash]', {'!hash': newHash});
+    } else {
+      this.log.warn('No new commit!');
+    }
+    console.log();
+
+    if (['standard', 'update'].includes(this.args.type)) {
+      this.log.note('Update composer');
+      await this.composer.install();
+      console.log();
+    }
+
+    if (this.args.type === 'standard') {
+      this.log.note('Update config');
+      await this.drush.cr();
+      await this.drush.cim(true);
+      console.log();
+    }
+
+    // gulp
+    await this.drush.cr();
+    this.log.success('Finished.');
+  } catch (error) {
+    this.log.failed('Failed with error:');
+    return {error: this.log.error(error)};
+  }
 };
-module.exports.params = [];
+module.exports.params = [
+  ['type', 'The type of the pull.', ['standard', 'update', 'speed'], 'standard'],
+];
 module.exports.description = 'Standard Deploy Script';
